39a2992ad89bbe6435315e1f46ab751dd9ddda3c
Amar kumar <aknbo30@gmail.com>
2024-11-29 00:10:37 +0530

    last-commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Route Visualize',
+      title: 'Route Visualize App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.purple,
